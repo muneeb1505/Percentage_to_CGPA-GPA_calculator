da5e05b69d0b5ff5d7c325d9e9ac355ac35c3919
muneeb1505 <150434834+muneeb1505@users.noreply.github.com>
2023-11-13 20:27:20 +0530

    Update per_to_conv.js

--- per_to_conv.js
+++ per_to_conv.js
@@ -1,1 +1,22 @@
+function calculate(){
+        const per = parseFloat(document.getElementById('per').value);
+        if(!isNaN(per)){
+ 
+            const cgpa = (per / 9.5).toFixed(2);
+            const gpa = ((per / 100) * 4).toFixed(2);
+            
+            document.getElementById('result').innerHTML = `CGPA: ${cgpa}`;
+            document.getElementById('result2').innerHTML = `GPA: ${gpa}`;
+            result.style.color = 'green';
+            result2.style.color = 'green';
 
+        }
+        else{
+
+            document.getElementById('result').innerHTML = 'Please enter a valid value!';
+            result.style.color = 'red';
+            document.getElementById('result2').innerHTML = 'Please enter a valid value!';
+            result2.style.color = 'red';
+
+        }
+    }
